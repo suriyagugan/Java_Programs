85fd35b3a2aaba04e1b8d0734bec15c0b1145469
ravisuriya <167037132+suriyagugan@users.noreply.github.com>
2024-10-22 05:26:28 +0530

    HelloWorld.java  File Created

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+class Hello
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World");
+        System.out.println("Welcome to Java Programs");
+    }
+}
+    
